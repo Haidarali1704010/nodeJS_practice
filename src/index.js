aa277fda205032ea6241a91fc18866fe0bf9213a
algorithmic_coder <haidarshaikh543@gmail.com>
2021-07-19 10:03:22 +0530

    Events in Node Js

--- src/index.js
+++ src/index.js
@@ -8,24 +8,55 @@
 // console.log(`Server is running on port : ${port}`);
 
 
-console.log("Working With files...");
-var fileManager = require("./fileManager");
-const fileName = __dirname + "/../data/temp.txt";
-fileManager.createNewFile(fileName);
+// console.log("Working With files...");
+// var fileManager = require("./fileManager");
+// const fileName = __dirname + "/../data/temp.txt";
+// fileManager.createNewFile(fileName);
 
-let jsonObject = {
-    food:[{
-        fname:"chicken",
-        fprice:"200",
-    },
-    {
-        fname:"paneer",
-        fprice:"150"
+// let jsonObject = {
+//     food:[{
+//         fname:"chicken",
+//         fprice:"200",
+//     },
+//     {
+//         fname:"paneer",
+//         fprice:"150"
+//     }
+// ],
+// resultCode:200,
+// restaurantName:"Taj Mahal"
+// };
+
+// fileManager.saveJsonToFile(jsonObject,fileName);
+// fileManager.readJsonObjectFromFile(fileName);
+
+const EventEmiiter = require("events");
+class Customer extends EventEmiiter {
+    constructor(name,gender){
+        super();
+        this.name = name;
+        this.gender = gender;
     }
-],
-resultCode:200,
-restaurantName:"Taj Mahal"
 };
+const mrJohn = new Customer("John","Male");
+const msMary = new Customer("Marry","Female");
 
-fileManager.saveJsonToFile(jsonObject,fileName);
-fileManager.readJsonObjectFromFile(fileName);
+const callBackFunction = (foods, customer) => {
+    for(let index in foods){
+        console.log(`${customer.name} calls ${foods[index]}`);
+    }
+}
+
+mrJohn.on("eventCallFoods",callBackFunction);
+msMary.on("eventCallFoods",callBackFunction);
+
+console.log("Do Something");
+console.log("Do Task");
+
+mrJohn.emit("eventCallFoods",["pizza","salad"],mrJohn);
+
+console.log("Do Something");
+console.log("Do something else");
+
+msMary.emit("eventCallFoods",["nugget","paneer"],msMary);
+
